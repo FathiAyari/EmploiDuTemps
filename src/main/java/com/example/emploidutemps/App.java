144824ab375e5692d90ea48b02d994647108f40c
FathiAyari <fathi.ayari@kelibia.r-iset.tn>
2024-12-24 14:21:02 +0100

    first screen done

--- src/main/java/com/example/emploidutemps/App.java
+++ src/main/java/com/example/emploidutemps/App.java
@@ -7,12 +7,13 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("first_screen.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(),900, 600);
+        stage.setResizable(false);
+        stage.setTitle("Emplois du temps UVT");
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,15 +1,11 @@
 module com.example.emploidutemps {
     requires javafx.controls;
     requires javafx.fxml;
-    requires javafx.web;
+    requires java.sql;
 
-    requires org.controlsfx.controls;
-    requires com.dlsc.formsfx;
-    requires net.synedra.validatorfx;
-    requires org.kordamp.ikonli.javafx;
-    requires org.kordamp.bootstrapfx.core;
-    requires com.almasb.fxgl.all;
+
 
     opens com.example.emploidutemps to javafx.fxml;
     exports com.example.emploidutemps;
 }
+
